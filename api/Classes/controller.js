ec6b520f3a4c80c9f6e3b76433206dfe703e0668
Gowthaman <gowthaman2185@gmail.com>
2023-02-04 18:40:40 +0530

    Class crud operations

--- api/Classes/controller.js
+++ api/Classes/controller.js
@@ -25,3 +25,58 @@ exports.allClass = async(req,res)=>{
     return res.status(400).json({status:'Failed',message:"An error occurred"})
   }
 }
+
+exports.getClassByID = async(req,res)=>{
+  try{
+    const className = await Class.findOne({
+      where:{
+        classCode:req.params.classID
+      }
+    })
+    return res.status(200).json({status:"Success",data:className})
+  } catch(error){
+    return res.status(400).json({status:"Failed",message:error.message})
+  }
+}
+
+exports.updateClass = async(req,res)=>{
+  try{
+    if(!req.body.classCode)
+      return res.status(400).json({status:'Failed',message:"A Class code in required"})
+    const updatedClass = await Class.findOne({
+      where:{
+        classCode:req.body.classCode
+      }
+    })
+
+    if(updatedClass){
+      for (let key in req.body)
+        if(key!=="classID" && updatedClass[key]!==req.body[key])
+          updatedClass[key] = req.body[key]
+      await updatedClass.save()
+      return res.status(400).json({status:"Success",data:updatedClass})
+    }
+    return res.status(400).json({status:'Failed',message:'Unable to find class'})
+  }catch(error){
+    return res.status(400).json({status:"Failed",message:error.message})
+  }
+}
+
+exports.deleteClass = async(req,res)=>{
+  try{
+    const deleteClass = await Class.findOne({
+      where:{
+        classCode:req.params.classID
+      }
+    })
+    if(deleteClass){
+      await deleteClass.destroy()
+      return res.status(200).json({status:"Success",message:"Class details deleted"})
+    }
+
+    return res.status(400).json({status:"Failed",message:"Unable to find class"})
+  }catch(error){
+    console.log(error)
+    return res.status(400).json({status:"Failed",message:error.message})
+  }
+}
--- api/Classes/router.js
+++ api/Classes/router.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { getAll,allClass } = require("./controller");
+const { getAll,allClass,getClassByID,updateClass,deleteClass } = require("./controller");
 
 router.get("/", getAll);
 router.post("/add",allClass);
+router.get("/:classID",getClassByID)
+router.post("/",updateClass)
+router.delete("/:classID",deleteClass)
 
 module.exports = router;
